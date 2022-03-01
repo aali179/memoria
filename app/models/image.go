7bed02d7908241df20ff107be1a648bdd8c288e2
Ruwani <ruwanide51@gmail.com>
2022-03-01 01:48:34 -0500

    chore: update db models

--- app/models/image.go
+++ app/models/image.go
@@ -1,9 +1,11 @@
 package models
+
 import (
 	"gorm.io/gorm"
 )
+
 type Image struct {
 	gorm.Model
-	ID int
-	File string
+	File []byte
+	Map  []Map
 }
--- app/models/page.go
+++ app/models/page.go
@@ -1,18 +1,17 @@
 package models
+
 import (
 	"gorm.io/gorm"
 )
 
 type Page struct {
 	gorm.Model
-	ID           int    `json:"id"`
-	Number       string `json:"number"`
-	ImageOne     Image    `json:"imageOne" gorm:"foreignKey:ID"`
-	ImageTwo     Image    `json:"imageTwo" gorm:"foreignKey:ID"`
-    ImageThree   Image    `json:"imageThree" gorm:"foreignKey:ID"`
-	HeadingOne   string `json:"headingOne"`
-	HeadingTwo   string `json:"headingTwo"`
-	HeadingThree string `json:"headingThree" `
-	Song         Song   `json:"song" gorm:"foreignKey:ID"`
-	Map         Map   `json:"map" gorm:"foreignKey:ID"`
+	Number       string  `json:"number"`
+	Images       []Image `gorm:"many2many:page_images;"`
+	HeadingOne   string  `json:"headingOne"`
+	HeadingTwo   string  `json:"headingTwo"`
+	HeadingThree string  `json:"headingThree" `
+	Song         Song    `json:"song"`
+	Map          Map     `json:"map"`
+	ScrapbookID  uint    `json:"scrapbook"`
 }
