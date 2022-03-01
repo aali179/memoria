aa5a33040bb9a47f6ae26df1bc0ba5a88b4594ec
Ruwani <ruwanide51@gmail.com>
2022-03-01 01:49:04 -0500

    feat: add get routes

--- app/routes.go
+++ app/routes.go
@@ -5,12 +5,26 @@ package main
 import (
 	"memoria/app/controllers"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
 func getMessage(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, "Hello World")
+}
+
+func getPage(c *gin.Context) {
+	pageID := c.Query("id")
+	id, err := strconv.ParseUint(pageID, 10, 32)
+	if err != nil {
+		c.JSON(500, "Something went wrong")
+	}
+	c.JSON(200, controllers.GetPage(uint(id)))
+}
+
+func createPage(c *gin.Context) {
+	c.JSON(200, controllers.CreatePage(c))
 }
 
 func searchSongs(c *gin.Context) {
@@ -28,5 +42,7 @@ func initializeRoutes(router *gin.Engine) {
 	router.GET("/", getMessage)
 	router.GET("/search", searchSongs)
 	router.GET("/song", getSong)
+	router.POST("/page", createPage)
+	router.GET("/page", getPage)
 
 }
